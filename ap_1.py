eb09dd6b708220e9310ab019bcb813b46099d1fb
GustavoViniciusSantos <61068093+GustavoViniciusSantos@users.noreply.github.com>
2020-09-16 11:11:12 -0300

    Add files via upload

---
+++ ap_1.py
@@ -0,0 +1,14 @@
+"""Jogo da memoria
+    Grupo 1 - Pedro Rocha, Beatriz Ferreira, Gustavo Santos
+"""
+
+def main():
+    """Ponto de entrada do jogo
+    """
+    # Inicializar o jogo
+    # Rodar o jogo
+    # Encerrar o jogo
+
+
+if __name__ == "__main__":
+    main()
